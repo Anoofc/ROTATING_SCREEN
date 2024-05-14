f9d75121df0551b8464563aaf3e1bae74e1e36bb
Anoof Chappangathil <anoof7262@gmail.com>
2024-05-14 17:54:58 +0400

    FIXED ALL THE MISTRIGGERING

---
+++ src/Backupu/main.cpp
@@ -0,0 +1,84 @@
+#define DEBUG 1
+
+#define STEP        10 // CHANGE AS REQUIRED
+#define MOTOR_ID    254
+#define BAUDRATE    9600
+#define GEAR_RATIO  4
+
+#include <SCServo.h>
+#include "Arduino.h"
+
+SCServo SERVO;
+
+uint8_t rot_flag = 0;
+uint16_t pos;
+uint16_t currentMotorAngle;
+uint16_t currentDiscAngle;
+uint16_t prevDiscAngle;
+uint16_t prevMotorAngle;
+uint16_t prevAngle = 0;
+
+void sendCMD()
+{
+  if (currentDiscAngle % STEP == 0)
+  { 
+    if (currentDiscAngle == 0 && prevDiscAngle == 360) { prevDiscAngle = 0; Serial.println("R");} // For correcting the direction of rotation
+    else if (currentDiscAngle == 360 && prevDiscAngle == 0) { prevDiscAngle = 360; Serial.println("L");} // For correcting the direction of rotation
+    else if (currentDiscAngle < prevDiscAngle) { Serial.println("L"); }
+    else if (currentDiscAngle > prevDiscAngle) { Serial.println("R"); }
+  }
+}
+
+
+void read_position()
+{
+  pos = SERVO.st_ReadPos(MOTOR_ID);
+  // Calculate the actual angle of rotation for the disc
+  currentMotorAngle = static_cast<float>(pos) / 4095 * 360 / GEAR_RATIO;
+}
+void update_motor_angle()
+{
+  if      (rot_flag == 0) { currentDiscAngle = currentMotorAngle; }
+  else if (rot_flag == 1) { currentDiscAngle = currentMotorAngle + 90; }
+  else if (rot_flag == 2) { currentDiscAngle = currentMotorAngle + 180; }
+  else if (rot_flag == 3) { currentDiscAngle = currentMotorAngle + 270; }
+}
+void update_rot_flag()
+{
+  if      ((rot_flag == 0) && (prevDiscAngle == 89)  && (currentMotorAngle == 0)) { rot_flag = 1; }
+  else if ((rot_flag == 1) && (prevDiscAngle == 179) && (currentMotorAngle == 0)) { rot_flag = 2; }
+  else if ((rot_flag == 2) && (prevDiscAngle == 269) && (currentMotorAngle == 0)) { rot_flag = 3; }
+  else if ((rot_flag == 3) && (prevDiscAngle == 359) && (currentMotorAngle == 0)) { rot_flag = 0; }
+
+  if ((rot_flag == 0) && (prevDiscAngle == 0)   && (currentMotorAngle == 89)) { rot_flag = 3; }
+  if ((rot_flag == 1) && (prevDiscAngle == 90)  && (currentMotorAngle == 89)) { rot_flag = 0; }
+  if ((rot_flag == 2) && (prevDiscAngle == 180) && (currentMotorAngle == 89)) { rot_flag = 1; }
+  if ((rot_flag == 3) && (prevDiscAngle == 270) && (currentMotorAngle == 89)) { rot_flag = 2; }
+}
+
+
+void setup()
+{
+  Serial.begin(BAUDRATE);
+  Serial2.begin(1000000);
+  SERVO.pSerial = &Serial2;
+  delay(500);
+  SERVO.EnableTorque(1, 0);
+  SERVO.EnableTorque(2, 1);
+}
+
+void loop()
+{
+  read_position();
+  update_rot_flag();
+  update_motor_angle();
+  sendCMD();
+
+  if (DEBUG)
+  {
+    Serial.println("Motor Position: " + String(pos) + "\t Motor Angle: " + String(currentMotorAngle) + "\t FLAG: " + String(rot_flag) + "\t Disc Angle: " + String(currentDiscAngle));
+  }
+
+  prevDiscAngle = currentDiscAngle;
+  prevMotorAngle = currentMotorAngle;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,32 @@
 #define DEBUG 0
 
-#define STEP        10 // CHANGE AS REQUIRED
-#define MOTOR_ID    254
-#define BAUDRATE    9600
-#define GEAR_RATIO  4
+#define STEP 30       // CHANGE AS REQUIRED
+#define MOTOR_ID 1
+#define BAUDRATE 9600
+#define GEAR_RATIO 4
 
-#include <SCServo.h>
-#include "Arduino.h"
+#include "SCServo.h"
 
 SCServo SERVO;
 
-uint8_t rot_flag = 0;
 uint16_t pos;
 uint16_t currentMotorAngle;
-uint16_t currentDiscAngle;
-uint16_t prevDiscAngle;
 uint16_t prevMotorAngle;
-uint16_t prevAngle = 0;
 
-void sendCMD()
-{
-  if (currentDiscAngle % STEP == 0)
-  { 
-    if (currentDiscAngle == 0 && prevDiscAngle == 360) { prevDiscAngle = 0; Serial.println("R");} // For correcting the direction of rotation
-    else if (currentDiscAngle == 360 && prevDiscAngle == 0) { prevDiscAngle = 360; Serial.println("L");} // For correcting the direction of rotation
-    else if (currentDiscAngle < prevDiscAngle) { Serial.println("L"); }
-    else if (currentDiscAngle > prevDiscAngle) { Serial.println("R"); }
+
+void sendCMD() {
+
+  if (currentMotorAngle % STEP == 0) {
+    if (currentMotorAngle == 1 && prevMotorAngle == 360) { prevMotorAngle = 0; Serial.println("R");} // For correcting the direction of rotation
+    if (currentMotorAngle == 360 && prevMotorAngle == 1) { prevMotorAngle = 360; Serial.println("L");}
+    if (currentMotorAngle < prevMotorAngle) {
+      Serial.println("L");
+    } else if (currentMotorAngle > prevMotorAngle) {
+      Serial.println("R");
+    }
   }
-}
 
-
-void read_position()
-{
-  pos = SERVO.st_ReadPos(MOTOR_ID);
-  // Calculate the actual angle of rotation for the disc
-  currentMotorAngle = static_cast<float>(pos) / 4095 * 360 / GEAR_RATIO;
 }
-void update_motor_angle()
-{
-  if      (rot_flag == 0) { currentDiscAngle = currentMotorAngle; }
-  else if (rot_flag == 1) { currentDiscAngle = currentMotorAngle + 90; }
-  else if (rot_flag == 2) { currentDiscAngle = currentMotorAngle + 180; }
-  else if (rot_flag == 3) { currentDiscAngle = currentMotorAngle + 270; }
-}
-void update_rot_flag()
-{
-  if      ((rot_flag == 0) && (prevDiscAngle == 89)  && (currentMotorAngle == 0)) { rot_flag = 1; }
-  else if ((rot_flag == 1) && (prevDiscAngle == 179) && (currentMotorAngle == 0)) { rot_flag = 2; }
-  else if ((rot_flag == 2) && (prevDiscAngle == 269) && (currentMotorAngle == 0)) { rot_flag = 3; }
-  else if ((rot_flag == 3) && (prevDiscAngle == 359) && (currentMotorAngle == 0)) { rot_flag = 0; }
-
-  if ((rot_flag == 0) && (prevDiscAngle == 0)   && (currentMotorAngle == 89)) { rot_flag = 3; }
-  if ((rot_flag == 1) && (prevDiscAngle == 90)  && (currentMotorAngle == 89)) { rot_flag = 0; }
-  if ((rot_flag == 2) && (prevDiscAngle == 180) && (currentMotorAngle == 89)) { rot_flag = 1; }
-  if ((rot_flag == 3) && (prevDiscAngle == 270) && (currentMotorAngle == 89)) { rot_flag = 2; }
-}
-
 
 void setup()
 {
@@ -69,16 +40,18 @@ void setup()
 
 void loop()
 {
-  read_position();
-  update_rot_flag();
-  update_motor_angle();
-  sendCMD();
+  pos = SERVO.st_ReadPos(MOTOR_ID);
+  // Calculate the actual angle of rotation for the disc
+  currentMotorAngle = map (pos, 1, 4095, 1 ,360);
 
-  if (DEBUG)
-  {
-    Serial.println("Motor Position: " + String(pos) + "\t Motor Angle: " + String(currentMotorAngle) + "\t FLAG: " + String(rot_flag) + "\t Disc Angle: " + String(currentDiscAngle));
+  if (DEBUG) {
+    Serial.print("Motor Position: ");
+    Serial.print(pos);
+    Serial.print("\t Motor Angle: ");
+    Serial.println(currentMotorAngle);
+    
   }
-
-  prevDiscAngle = currentDiscAngle;
+  sendCMD();
   prevMotorAngle = currentMotorAngle;
+
 }
